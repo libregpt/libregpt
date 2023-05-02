5eb7a931bcc32217eaace01d8b4210020a22256e
Samuele Della Vedova <46026355+samueledellavedova@users.noreply.github.com>
2023-05-02 15:57:14 +0200

    feat: add basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-  println!("bruh");
+use std::convert::Infallible;
+use hyper::{Body, Request, Response, Server, service};
+use tracing::{error, info};
+
+#[tokio::main]
+async fn main() {
+  tracing_subscriber::fmt::init();
+
+  let addr = "0.0.0.0:80".parse().unwrap();
+  let make_service = service::make_service_fn(|_| async move {
+    Ok::<_, Infallible>(service::service_fn(|_: Request<Body>| async move {
+      Ok::<_, Infallible>(Response::new(Body::from("pp")))
+    }))
+  });
+
+  let server = Server::bind(&addr).serve(make_service);
+
+  info!("listening on {addr}");
+
+  if let Err(err) = server.await {
+    error!("server died: {err}");
+  }
 }
